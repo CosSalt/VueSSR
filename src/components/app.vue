cd35f06bc491162d9d1bdc0066a8435426896355
HoSalt <ihosalt@gmail.com>
2018-03-06 12:59:06 +0800

    vue ssr 测试代码，还不知道怎么用，先按照官方教程写

---
+++ src/components/app.vue
@@ -0,0 +1,15 @@
+//第 1 步：创建一个 Vue 实例
+const Vue = require('vue')
+
+const app = new Vue({
+	template: `<div>Hello Word</div>`
+})
+
+//第 2 步：创建一个renderer
+const renderer = require('vue-server-renderer').createRenderer();
+
+//第 3 步：将 Vue 实例渲染为 HTML
+renderer.renderToString(app,(err,html)=>{
+	if(err) throw err
+	console.log(html)
+})
