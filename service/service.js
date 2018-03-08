579943ee05a04bfcfdf23f6613dd4145a7183735
HoSalt <ihosalt@gmail.com>
2018-03-08 13:22:57 +0800

    通过模版生成 HTML 代码

--- service/service.js
+++ service/service.js
@@ -1,7 +1,24 @@
 const Vue= require('vue')
 const server = require('express')()
+const vueServerRenderer =require('vue-server-renderer');
+
 const renderer = require('vue-server-renderer').createRenderer()
-server.get('*',(req,res)=>{
+server.get("/a",(req,res)=>{
+  const renderer2 = vueServerRenderer.createRenderer({
+    template: require('fs').readFileSync('./src/components/index.template.html','utf-8')
+  })
+  const app = new Vue({
+    data:{
+      url: req.url
+    },
+    template:`<div>你想要访问的 URL 是：{{ url}}</div>`
+  })
+  renderer2.renderToString(app,(err,html)=>{
+    console.log(html);
+    res.end(html)
+  })
+})
+server.get('/*',(req,res)=>{
   var a={"Content-Type": "text/plain;charset=utf-8"};
   res.set('Content-Type', 'text/html;charset=utf-8');
   //res.set(a);
